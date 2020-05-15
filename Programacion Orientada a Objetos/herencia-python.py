c479a4372a29b701498ee1fcf43bb88d290ff03f
kenderperez <kenderperez1995@gmail.com>
2020-05-15 01:56:54 -0400

    Update herencia-python.py

--- Programacion Orientada a Objetos/herencia-python.py
+++ Programacion Orientada a Objetos/herencia-python.py
@@ -4,9 +4,20 @@ class EspadaMarina(Espada):
 	def ataqueTsunami(self, objetivo):
 		
 		#verificamos que el objetivo sea de tipo marino para poder restarle puntos de vida 
-		if objetivo[‘tipo’] == ‘marino’:
+		if objetivo[‘'tipo'] == 'marino':
 			#restamos al objetivo 10 puntos de vida pero sumamos su resistencia
 			objetivo['vida'] = objetivo['vida'] - 10 + objetivo['resistencia']
 		else:
 			#si el objetivo no es de tipo marino mostramos el siguiente mensaje
-			print(‘este ataque solo funciona contra enemigos marinos!!’)
+			print('este ataque solo funciona contra enemigos marinos!!')
+
+			
+#creamos la espada pirata la cual contara con el nuevo ataque
+espadaPirata = EspadaMarina('Espada Pirata', 'rara', 10, 7) 
+
+#creamos la espada de Poseidon la cual contara con el nuevo ataque
+espadaDePoseidon = EspadaMarina('Espada de Poseidon', 'epica', 12, 10) 
+
+#atacamos al kraken primero con el ataque normal y luego con es ataque especial que creamos
+espadaDePoseidon.ataque(kraken)
+espadaDePoseidon.ataqueTsunami(kraken)
